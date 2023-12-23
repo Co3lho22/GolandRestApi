c2908fe0f6c09c37089a7901005dce20809760d6
up202107596 <up202107596@edu.fc.up.pt>
2023-12-23 23:35:28 +0000

    correct a error from the sql

--- pkg/repository/dbRBAC.go
+++ pkg/repository/dbRBAC.go
@@ -21,7 +21,8 @@ func GetPermissionsByRoleId(logger *logrus.Logger, db *sql.DB, roleId int) ([]st
 
 func GetUserRolesByUserId(logger *logrus.Logger, db *sql.DB, userId int) ([]string, error) {
 	var roles []string
-	query := "SELECT r.* FROM ROLE r INNER JOIN USER_ROLE ur ON r.id = ur.role_id WHERE ur.user_id = ?"
+	//query := "SELECT r.* FROM ROLE r INNER JOIN USER_ROLE ur ON r.id = ur.role_id WHERE ur.user_id = ?"
+	query := "SELECT r.name FROM ROLE r INNER JOIN USER_ROLE ur ON r.id = ur.role_id WHERE ur.user_id = ?"
 	err := db.QueryRow(query, userId).Scan(&roles)
 	if err != nil {
 		logger.WithError(err).WithField("userId", userId).Error("Error retrieving permissions using roleId")
